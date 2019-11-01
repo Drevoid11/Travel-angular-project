f3c06149addf95dbd5a0dd01db0fe48615b54f60
Drevoid11 <paranjape.chinmay@outlook.com>
2019-11-01 15:01:15 +0530

    4 files modified, 8 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule, RouterModule.forRoot([
+    {path: '', component : HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+  ])  ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, ContactComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
